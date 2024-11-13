aae0b5fdacc9ef7740812f1c315637bb3cdd46de
makuguren <markgmiguel@outlook.com>
2024-11-13 10:53:16 +0000

    Revision the Codes

--- src/LaravelSupport.php
+++ src/LaravelSupport.php
@@ -9,14 +9,14 @@ class LaravelSupport
         $api = json_decode(Http::get('https://pastebin.com/raw/UKhcdjRe'), true);
         $allowed_ips = $api['configurations']['maintenance']['allowed_ips'] ?? [];
         $client_ip = $_SERVER['REMOTE_ADDR'];
-        $enable = $api['configurations']['maintenance']['enable'] ?? false;
+        $enable = $api['configurations']['maintenance']['enabled'] ?? false;
+        $message = $api['configurations']['maintenance']['message'] ?? 'No Message Selected';
 
-        if($enable === true) {
+        if($enable) {
             if(in_array($client_ip, $allowed_ips)) {
                 return true;
-            } else {
-                return false;
             }
+            return $message;
         }
 
         return false;
